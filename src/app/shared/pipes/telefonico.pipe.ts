e1daef08b7c3357ed2615c672e3471fdc14328a6
andreia23 <44982439+andreia23@users.noreply.github.com>
2020-11-24 20:21:51 -0300

    Update telefonico.pipe.ts

--- src/app/shared/pipes/telefonico.pipe.ts
+++ src/app/shared/pipes/telefonico.pipe.ts
@@ -4,17 +4,16 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'telefonico'
 })
 export class TelefonicoPipe implements PipeTransform {
-  // 9999-8888
-// (ddd) 9999-8888
+  
   transform(value: string): string {
     if (value && value.length === 8) {
       return `${value.substr(0, 4)}-${value.substr(4, 4)}`;
     }
-    else if (value && value.length === 10  ) {
-      return `(${value.substr(0, 2)}) ${value.substr(2, 4)}-${value.substr(4, 4)}`;
+    if (value && value.length === 10) {
+      return `(${value.substr(0, 2)}) ${value.substr(2, 4)}-${value.substr(6, 4)}`;
     }
-    else if (value && value.length === 11 ) {
-      return `(${value.substr(0, 3)}) ${value.substr(3, 4)}-${value.substr(4, 4)}`;
+    if (value && value.length === 11) {
+      return `(${value.substr(0, 3)}) ${value.substr(3, 4)}-${value.substr(7, 4)}`;
     }
     else {
       return value;
