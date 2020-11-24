7886b70470df6f3ace345ba70d1a3e99f893d11e
andreia23 <44982439+andreia23@users.noreply.github.com>
2020-11-24 20:22:50 -0300

    Update telefonico.pipe.ts

--- src/app/shared/pipes/telefonico.pipe.ts
+++ src/app/shared/pipes/telefonico.pipe.ts
@@ -9,10 +9,10 @@ export class TelefonicoPipe implements PipeTransform {
     if (value && value.length === 8) {
       return `${value.substr(0, 4)}-${value.substr(4, 4)}`;
     }
-    if (value && value.length === 10) {
+    else if (value && value.length === 10) {
       return `(${value.substr(0, 2)}) ${value.substr(2, 4)}-${value.substr(6, 4)}`;
     }
-    if (value && value.length === 11) {
+    else if (value && value.length === 11) {
       return `(${value.substr(0, 3)}) ${value.substr(3, 4)}-${value.substr(7, 4)}`;
     }
     else {
